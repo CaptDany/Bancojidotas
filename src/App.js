69abaadfbd1c0c2b5b3b61a32e5628a61e4d5383
AlanBarucq <st3659@utr.edu.mx>
2024-02-29 12:28:07 -0600

    Funcion 3. Registro, modificación y eliminación de tarjetas de débito y crédito agregada

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
   return (
@@ -6,6 +7,36 @@ function App() {
 
     </div>
   );
+}
+
+function Cards(){ //Tarjetas de credito y debito
+  const [accounts, setAccounts] = useState({});
+  const [cards, setCards] = useState({});
+
+  const addCard = (cardNumber, accountNumber) => {
+      if (!(accountNumber in accounts)) {
+          console.log("Account does not exist.");
+      } else if (cardNumber in cards) {
+          console.log("Card already exists.");
+      } else {
+          setCards(prevCards => ({
+              ...prevCards,
+              [cardNumber]: { cardNumber, accountNumber }
+          }));
+          console.log("Card added successfully.");
+      }
+  };
+
+  const deleteCard = (cardNumber) => {
+      if (!(cardNumber in cards)) {
+          console.log("Card does not exist.");
+      } else {
+          const updatedCards = { ...cards };
+          delete updatedCards[cardNumber];
+          setCards(updatedCards);
+          console.log("Card deleted successfully.");
+      }
+  };
 }
 
 export default App;

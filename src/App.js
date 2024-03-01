cde796d37c8505ee81bd16b6af88ada4a0df0ecb
Volcanoacid <alexismartinez3122@yahoo.com>
2024-02-29 21:59:34 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,9 @@ import React, { useState } from 'react';
 function App() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [regUser, setRegUser] = useState('');
+  const [regPass, setRegPass] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(username, password);
-  }
 
   return (
   <signIn>
@@ -20,27 +18,31 @@ function App() {
         <h1>Register User</h1>
       </div>
       <div>
-        <form onSubmit={handleSubmit}>
+        <form>
           <label for="username">Username</label><br/>
           <input 
             value={username}
             onChange={e => {
             setUsername(e.target.value);
-            }}
-            /><br/>
+            }}/>
+            <br/>
           <label for="pass">Password</label><br/>
-          <input 
-            
+          <input
             onChange={e => {
               setPassword(e.target.value)
-            }}
-            /><br/>
-          <button type='submit'>
+            }}/>
+            <br/>
+          <button type='submit' onClick={(e) => {
+            e.preventDefault();
+            setRegUser(username);
+            setRegPass(password);
+            console.log('username: ',username,'password: ', password,'test: ', test);
+          }}>
           submit
           </button><br/>
           <h1>
-            Username: {username}<br/>
-            Password: {password}
+            Registered Username: {regUser}<br/>
+            Registered Password: {regPass}<br/>
           </h1>
         </form>
       </div>
